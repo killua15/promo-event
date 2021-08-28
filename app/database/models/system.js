d9fd827f55782894b3ae0812360be930d4421556
David Lopez <dlopezsalinas152@gmail.com>
2021-08-28 16:31:29 -0400

    mi primera migracion sequelize

---
+++ app/database/models/system.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class System extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  system.init({
+    name: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    },
+    status: {
+      type:DataTypes.BOOLEAN,
+      defaultValue:true
+    },
+    updatedUser: {
+      type:DataTypes.STRING,
+      allowNull:false,
+    }
+  }, {
+    sequelize,
+    modelName: 'System',
+    tableName:'systems'
+  });
+  return System;
+};
