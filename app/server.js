64015ef3e2e5792fd098911d863f73971b2899c5
David Lopez <dlopezsalinas152@gmail.com>
2022-01-07 16:12:21 -0500

    Fix Limit body Parser

--- app/server.js
+++ app/server.js
@@ -1,19 +1,15 @@
 const express = require("express");
-var bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
 const port = process.env.PORT || 3000;
 const { sequelize } = require("./database/models");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 app.use(require("./routes"));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.limit(100000000));
+
 app.listen(port, () => {
   console.log("Running is running in", port);
 
