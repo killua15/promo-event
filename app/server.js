a8f1a0d2c421700183eac5d90772712aef76c05b
David Lopez <dlopezsalinas152@gmail.com>
2022-01-07 15:56:04 -0500

    installing bodyparser

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 3000;
 const { sequelize } = require("./database/models");
 
